910038fccefacd9f306f58a8231974b79f27b01e
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-04-07 16:06:54 +0530

    update

--- SpringCore/src/main/java/com/SpringCore/App.java
+++ SpringCore/src/main/java/com/SpringCore/App.java
@@ -11,10 +11,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      
         ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
         Student  stud1=(Student) context.getBean("student1");
+        Student  stud2=(Student) context.getBean("student2");
+        Student  stud3=(Student) context.getBean("student3");
         System.out.println(stud1);
+        System.out.println(stud2);
+        System.out.println(stud3);
         
     }
 }
