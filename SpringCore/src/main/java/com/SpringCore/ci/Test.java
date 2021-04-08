5ad211a72f334674ee2c2e7279f646ea413a7e04
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-04-08 11:39:24 +0530

    update

---
+++ SpringCore/src/main/java/com/SpringCore/ci/Test.java
@@ -0,0 +1,16 @@
+package com.SpringCore.ci;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context=new  ClassPathXmlApplicationContext("com/SpringCore/ci/ciconfig.xml");
+	    Pesron p =(Pesron) context.getBean("person1");
+	    System.out.println(p);
+	   
+	}
+
+}
