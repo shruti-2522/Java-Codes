89ba2c5bae99ee942394876a3f8d43386b64c5b2
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-04-10 12:21:27 +0530

    update

--- SpringCore/src/main/java/com/SpringCore/sterotype/Student.java
+++ SpringCore/src/main/java/com/SpringCore/sterotype/Student.java
@@ -3,9 +3,11 @@ package com.SpringCore.sterotype;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Student {
 
 	@Value("shruti")
