d92e41b7341e3e373cd7c19526505c2d8bbb6ae0
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-04-19 12:12:07 +0530

    Sending data controller to view using model

--- springmvc/src/main/java/springmvc/controller/HomeController.java
+++ springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -1,14 +1,28 @@
 package springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("This is Home Url..");
+		model.addAttribute("name","Shruti Amrutkar");
+		model.addAttribute("id",1);
+		
+		List<String> frnds=new ArrayList<String>();
+		frnds.add("Sairaj");
+		frnds.add("Niki");
+		frnds.add("Hitesh");
+		frnds.add("Rushali");
+		
+		model.addAttribute("f",frnds);
 		return "index";
 	}
 
--- springmvc/src/main/webapp/WEB-INF/views/index.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,3 +1,4 @@
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
@@ -11,5 +12,31 @@
 	<h1>This is Home Page</h1>
 	<h1>Controlled by Home controller</h1>
 	<h1>URL /home</h1>
+	<%
+	String name = (String) request.getAttribute("name");
+	Integer id = (Integer) request.getAttribute("id");
+	List<String> frnds = (List<String>) request.getAttribute("f");
+	%>
+
+	<h1>
+		Name is
+		<%=name%></h1>
+	<h1>
+		ID is
+		<%=id%></h1>
+		
+		
+		<%
+		  for(String s:frnds)
+		  {
+			  %>
+			  
+		<h1><%=s %></h1>
+		
+		
+			  <%
+			  
+		  }
+		%>
 </body>
 </html>
