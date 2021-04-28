ccdddc499f7959e16d467cdb9112a3ab16674e4a
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-04-28 13:07:49 +0530

    update

--- spingRestapi/src/main/java/com/Restapi/book/controller/BookController.java
+++ spingRestapi/src/main/java/com/Restapi/book/controller/BookController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,34 +20,47 @@ import com.Restapi.book.services.BookService;
 
 @RestController
 public class BookController {
-	
+
 	@Autowired
 	private BookService bookService;
-	//@RequestMapping(value="/books",method=RequestMethod.GET)
-	//@ResponseBody
+	// @RequestMapping(value="/books",method=RequestMethod.GET)
+	// @ResponseBody
+
+	// get ALL Book Handler
 	@GetMapping("/books")
-	public List<Book> getBook()
-	{
-		
+	public List<Book> getBook() {
+
 		/*
 		 * Book b=new Book(); b.setBid(1); b.setBname("Java Frameowrks");
 		 * b.setAuthor("Durgesh Tiwari");
 		 */
 		return this.bookService.getAllBook();
 	}
-	
+
+	// get Single book Hnadler
 	@GetMapping("/books/{id}")
-	public Book getBook(@PathVariable("id")int id)
-	{
+	public Book getBook(@PathVariable("id") int id) {
 		return bookService.getBookById(id);
 	}
-	
-	//Create
+
+	// Create Book Hnadler
 	@PostMapping("/books")
-	public Book addBook(@RequestBody Book b1)
-	{
-		Book b=this.bookService.addBook(b1);
+	public Book addBook(@RequestBody Book b1) {
+		Book b = this.bookService.addBook(b1);
 		return b;
 	}
 
+	// Delete book handler:
+	@DeleteMapping("/books/{id}")
+	public void delBook(@PathVariable("id") int id) {
+		this.bookService.delBook(id);
+	}
+
+	// Update Book Handler
+	@PutMapping("/books/{id}")
+	public Book updatBook(@RequestBody Book book, @PathVariable("id") int id) {
+
+		this.bookService.updateBook(book, id);
+		return book;
+	}
 }
--- spingRestapi/src/main/java/com/Restapi/book/services/BookService.java
+++ spingRestapi/src/main/java/com/Restapi/book/services/BookService.java
@@ -2,45 +2,68 @@ package com.Restapi.book.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.Restapi.book.entities.Book;
 
 @Component
 public class BookService {
 
-	private static List<Book> list=new ArrayList<>();
-	
-	static
-	{
-		list.add(new Book(1,"SADP","Darekar Sir"));
-		list.add(new Book(2,"Machine Learning","Deore mam"));
-		list.add(new Book(3,"Web Framework","Shewale mam"));
+	private static List<Book> list = new ArrayList<>();
+
+	static {
+		list.add(new Book(1, "SADP", "Darekar Sir"));
+		list.add(new Book(2, "Machine Learning", "Deore mam"));
+		list.add(new Book(3, "Web Framework", "Shewale mam"));
 	}
-	
-	
-	//get all books
-	
-	public List<Book> getAllBook(){
+
+	// get all books
+
+	public List<Book> getAllBook() {
 		return list;
-		
+
 	}
-	
-	//get Single book by id
-	public Book getBookById(int id)
-	{
-		Book b1=null;
-		b1=list.stream().filter(e->e.getBid()==id).findFirst().get();
+
+	// get Single book by id
+	public Book getBookById(int id) {
+		Book b1 = null;
+		b1 = list.stream().filter(e -> e.getBid() == id).findFirst().get();
 		return b1;
 	}
-	
-	//Add Book
-	public  Book addBook(Book b)
-	{
+
+	// Add Book
+	public Book addBook(Book b) {
 		list.add(b);
 		return b;
 	}
-	
-	
+
+	// Delete Book
+
+	public void delBook(int id) {
+		list = list.stream().filter(book -> {
+			if (book.getBid() != id) {
+				return true;
+			} else {
+				return false;
+			}
+
+		}).collect(Collectors.toList());
+
+	}
+
+	// Update Book
+	@PutMapping("/book/{id}")
+	public void updateBook(Book b, int id) {
+		list = list.stream().map(b1 -> {
+			if (b.getBid() == id) {
+				b.setBname(b.getBname());
+				b.setAuthor(b.getAuthor());
+			}
+			return b;
+		}).collect(Collectors.toList());
+	}
+
 }
